1b3aa7e6c88cd7a3232741cf933ee7e62ee84049
Root <onr82091@gmail.com>
2026-03-19 01:25:03 +0100

    Update author name in user script metadata

--- JKLM-Power-Tools.user.js
+++ JKLM-Power-Tools.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2.6
 // @description  Advanced JKLM Power Tools with Dictionary, Notes and UI Customization
-// @author       Rooticles
+// @author       Root
 // @updateURL    https://raw.githubusercontent.com/rooticles/JKLM-Power-Tools/main/JKLM-Power-Tools.user.js
 // @downloadURL  https://raw.githubusercontent.com/rooticles/JKLM-Power-Tools/main/JKLM-Power-Tools.user.js
 // @match        *://*.jklm.fun/*
